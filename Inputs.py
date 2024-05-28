a5a9fecaea776bf667380bf155e704d5121ec6a0
Luigi Erignoux <166017590+Waluigizzz@users.noreply.github.com>
2024-05-28 18:32:32 +0200

    Update Inputs.py

--- Inputs.py
+++ Inputs.py
@@ -4,6 +4,33 @@ import matplotlib.pyplot as plt
 
 import dm4bem
 
+# Data building
+# =========================================================================
+# Dimensions
+L1, L2, c1, c2 = 4, 6, 3, 4  # m
+H = 3        # m
+H_vitre = 1   # m
+L_vitre = 1  # m
+W_mur_ext = 0.30   # m
+w_mur_ext_beton = 0.20   # m
+w_mur_ext_insulation = 0.1 # m
+w_mur_int = 0.3 # m
+w_vitre = 0.02 # m
+
+# Pièce B prend en compte w_int !!!!
+
+# Surfaces
+S_A_mur_ext = (L1+(c1-L_vitre))*H + L_vitre*(H-H_vitre) + W_mur_ext*2*H
+S_A_mur_int = S_A_mur_ext - W_mur_ext*2*H
+S_B_mur_ext = ((L2-L_vitre)+c1+2*W_mur_ext+w_mur_int)*H + L_vitre*(H-H_vitre)
+S_B_mur_int = (S_B_mur_ext - 2*W_mur_ext+w_mur_int)*H
+S_C_mur_ext = (L1+L2+w_mur_int-2*L_vitre+2*c2+4*W_mur_ext)*H + 2*L_vitre*(H-H_vitre)
+S_C_mur_int = S_C_mur_ext - 4*W_mur_ext*H
+S_fenetre = H_vitre*L_vitre
+S_AB = c1*H
+S_AC = L1*H
+S_BC = L2*H
+
 # Inputs
 # =========================================================================
 
@@ -39,14 +66,13 @@ Ti_sp = pd.Series(
 
 # Flow rate sources
 # Total solar irradiance
-W_mur_ext = 0.30   # m
+surface_orientation = {'slope': 90,    # 90° is vertical, 90° downward
+                       'azimuth': 0,    # 0° South, positive westward
+                       'latitude': 45}    # °, North Pole 90° positive
 
-surface_orientation = {'slope': w0['β'].values[0],
-                       'azimuth': w0['γ'].values[0],
-                       'latitude': 45}
-
+albedo = 0.2
 rad_surf = dm4bem.sol_rad_tilt_surf(
-    weather, surface_orientation, w0['albedo'].values[0]) 
+    weather, surface_orientation, albedo) 
 
 Etot = rad_surf.sum(axis=1)
 
@@ -55,14 +81,18 @@ Etot = rad_surf.sum(axis=1)
 τ_gSW = 0.30    # short wave transmitance: reflective blue glass
 S_g = 9         # m², surface area of glass
 
+# Outdoor wall properties
+α0_wSW = 0.25    # short wave absortivity: indoor white smooth surface
+α1_wSW = 0.30    # short wave absortivity: outdoor concrete wall
+
 # Solar radiation absorbed by the outdoor surface of the wall
-Φo = w0['α1'].values[0] * w0['Area'].values[0] * Etot
+Φo = α1 * (S_A_mur_ext + S_B_mur_ext +S_C_mur_ext) * Etot
 
 # Solar radiation absorbed by the indoor surface of the wall
-Φi = τ_gSW * w0['α0'].values[0] * S_g * Etot
+Φi = τ_gSW * α0 * S_fenetre * Etot
 
 # Solar radiation absorbed by the glass
-Φa = α_gSW * S_g * Etot
+Φa = α_gSW * S_fenetre * Etot
 
 # Auxiliary (internal) sources
 Qa = 0 * np.ones(weather.shape[0])
@@ -72,5 +102,4 @@ input_data_set = pd.DataFrame({'To': To, 'Ti_sp': Ti_sp,
                                'Φo': Φo, 'Φi': Φi, 'Qa': Qa, 'Φa': Φa,
                                'Etot': Etot})
 
-input_data_set.to_csv('./toy_model/input_data_set.csv')
 print(input_data_set)
